b540b26d9dbe39835886469917f1df069d5851e2
akishino-007 <akishino1953@gmail.com>
2025-06-16 15:44:58 +0900

    change sql

--- j_project/settings.py
+++ j_project/settings.py
@@ -76,11 +76,22 @@ WSGI_APPLICATION = 'j_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+      'ENGINE':"django.db.backends.postgresql_psycopg2",
+      'NAME':"mydatabase",
+      'USER':"akishino",
+      'PASSWORD':"akishino007sql",
+      'HOST':"localhost",
+      'PORT':"5432"
+   }
 }
 
 if not DEBUG:
