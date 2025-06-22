563cb915858184e8453bf045beaa9a5d3b5614ae
akishino-007 <akishino1953@gmail.com>
2025-06-22 15:10:29 +0900

    allauth introduction

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,7 +2,17 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser  
 
-if not admin.site.is_registered(CustomUser):
-    admin.site.register(CustomUser, UserAdmin)
+#if not admin.site.is_registered(CustomUser):
+#    admin.site.register(CustomUser, UserAdmin)
 
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        ("追加情報", {"fields": ("bio", "birthyear", "kumi")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("追加情報", {"fields": ("bio", "birthyear", "kumi")}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
 
